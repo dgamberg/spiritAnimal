6b59399a777455cd95fad0029b43e15f8e3ea323
Dave Gamberg <dgamberg@gmail.com>
2015-11-02 17:03:03 -0600

    adding animation

--- server/public/assets/scripts/app.js
+++ server/public/assets/scripts/app.js
@@ -2,7 +2,9 @@ $(document).ready(function(){
     $('#search').submit(function(event){
 
         event.preventDefault();
+
         $('#outputContainer').empty();
+
         var values = {};
 
         $.each($(this).serializeArray(), function(i, field){
@@ -26,8 +28,8 @@ function appendToDom(data){
     for( var i = 0; i< data.length; i++){
         $('#outputContainer').append("<div class='data-box'></div>");
         var $el = $('#outputContainer').children().last();
-        $el.append("<div class='people-container'><p class='people'>"  + data[i].name + "</p></div>");
-        $el.append("<div class='animal-container'><p class='animal'>"  + data[i].spiritAnimal + "</p></div>");
+        $el.append("<div class='people-container'><p class='people'>"  + data[i].name + "</p></div>").hide().slideDown("slow");
+        $el.append("<div class='animal-container'><p class='animal'>"  + data[i].spiritAnimal + "</p></div>").hide().slideDown("slow");
     }
 
 }
